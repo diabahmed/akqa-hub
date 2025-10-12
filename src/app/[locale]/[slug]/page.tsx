e797c10ab40f58b7322b4e30bd047cf8312c1ffc
diabahmed <ahmillect@outlook.com>
2025-10-12 02:38:17 +0200

    feat: update page layout with custom pattern and refined article components

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next';
 import { draftMode } from 'next/headers';
 import { notFound } from 'next/navigation';
 
-import { ArticleContent, ArticleHero, ArticleTileGrid } from '@src/components/features/article';
+import Pattern from '@src/components/custom/pattern';
+import { ArticleContent, ArticleHero } from '@src/components/features/article';
 import { Container } from '@src/components/shared/container';
-import initTranslations from '@src/i18n';
 import { defaultLocale, locales } from '@src/i18n/config';
 import { client, previewClient } from '@src/lib/client';
 
@@ -71,12 +71,10 @@ export default async function Page({ params }: BlogPageProps) {
   const { locale, slug } = await params;
   const { isEnabled: preview } = await draftMode();
   const gqlClient = preview ? previewClient : client;
-  const { t } = await initTranslations({ locale });
   const { pageBlogPostCollection } = await gqlClient.pageBlogPost({ locale, slug, preview });
   const { pageLandingCollection } = await gqlClient.pageLanding({ locale, preview });
   const landingPage = pageLandingCollection?.items[0];
   const blogPost = pageBlogPostCollection?.items[0];
-  const relatedPosts = blogPost?.relatedBlogPostsCollection?.items;
   const isFeatured = Boolean(
     blogPost?.slug && landingPage?.featuredBlogPost?.slug === blogPost.slug,
   );
@@ -87,18 +85,13 @@ export default async function Page({ params }: BlogPageProps) {
 
   return (
     <>
-      <Container>
-        <ArticleHero article={blogPost} isFeatured={isFeatured} isReversedLayout={true} />
+      <Container className="max-w-4xl">
+        <ArticleHero article={blogPost} isFeatured={isFeatured} variant="flat" />
       </Container>
-      <Container className="mt-8 max-w-4xl">
+      <Container className="-pb-2 max-w-4xl pt-8 pb-0">
         <ArticleContent article={blogPost} />
       </Container>
-      {relatedPosts && (
-        <Container className="mt-8 max-w-5xl">
-          <h2 className="mb-4 md:mb-6">{t('article.relatedArticles')}</h2>
-          <ArticleTileGrid className="md:grid-cols-2" articles={relatedPosts} />
-        </Container>
-      )}
+      <Pattern />
     </>
   );
 }
